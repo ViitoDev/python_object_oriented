0c70f401210d91faa22363a8bb686536b7dc9576
ViitoDev <joaovictorpiresdev@gmail.com>
2026-01-30 09:45:36 -0300

    FEAT - Class method

--- Python class/restaurant.py
+++ Python class/restaurant.py
@@ -2,24 +2,29 @@ class Restaurant:
     restaurants = []
 
     def __init__(self, name, category):
-        self.name = name
-        self.category = category
+        self._name = name.title()
+        self._category = category.upper()
         self._active = False
         Restaurant.restaurants.append(self)
 
     def __str__(self):
-        return f"{self.name} | {self.category}"
+        return f"{self._name} | {self.category}"
     
-    def show_restaurants():
+    @classmethod
+    def show_restaurants(cls):
         print(f"{"Restaurant name".ljust(25)} | {"Category".ljust(25)} | {"Status"}")
-        for restaurant in Restaurant.restaurants:
-            print(f"{restaurant.name.ljust(25)} | {restaurant.category.ljust(25)} | {restaurant.active}")
+        for restaurant in cls.restaurants:
+            print(f"{restaurant._name.ljust(25)} | {restaurant._category.ljust(25)} | {restaurant.active}")
 
     @property
     def active(self):
         return "☒" if self._active else "☐"
+    
+    def state_alternate(self):
+        self._active = not self._active
 
 restaurant_rossi = Restaurant("rossi", "italian")
+restaurant_rossi.state_alternate()
 restaurant_vito = Restaurant("vito", "pizzeria")
 
 Restaurant.show_restaurants()
