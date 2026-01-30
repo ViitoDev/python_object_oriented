85692ac6604178400424b72d0633984655a393b7
ViitoDev <joaovictorpiresdev@gmail.com>
2026-01-30 09:30:54 -0300

    FEAT - Adding property

--- Python class/restaurant.py
+++ Python class/restaurant.py
@@ -4,15 +4,20 @@ class Restaurant:
     def __init__(self, name, category):
         self.name = name
         self.category = category
-        self.active = False
+        self._active = False
         Restaurant.restaurants.append(self)
 
     def __str__(self):
         return f"{self.name} | {self.category}"
     
     def show_restaurants():
+        print(f"{"Restaurant name".ljust(25)} | {"Category".ljust(25)} | {"Status"}")
         for restaurant in Restaurant.restaurants:
-            print(f"{restaurant.name} | {restaurant.category} | {restaurant.active}")
+            print(f"{restaurant.name.ljust(25)} | {restaurant.category.ljust(25)} | {restaurant.active}")
+
+    @property
+    def active(self):
+        return "☒" if self._active else "☐"
 
 restaurant_rossi = Restaurant("rossi", "italian")
 restaurant_vito = Restaurant("vito", "pizzeria")
